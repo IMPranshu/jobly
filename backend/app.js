ab54db9c6d76ac69e8768a4409c336414ad63fde
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-10-22 10:28:40 +0530

    add custom "errorHandler" middleware

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 require("dotenv").config();
+const cookieParser = require("cookie-parser"); // for authentication
+const errorHandler = require("./middleware/error");
 
 //database connection
 mongoose
@@ -13,6 +17,21 @@ mongoose
   })
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
+
+//Middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true,
+  })
+);
+app.use(cookieParser());
+app.use(cors());
+
+// errorHandler middleware
+app.use(errorHandler);
 
 // port for the application
 const port = process.env.PORT || 9000;
