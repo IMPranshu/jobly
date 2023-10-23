f3c446e1709ee55a0e92e66b52b0d42d942ddbb9
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-10-23 23:13:30 +0530

    add user dashboard page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,15 @@ import { theme } from "./theme";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import LogIn from "./pages/LogIn";
+import UserDashboard from "./pages/user/UserDashboard";
+import UserRoute from "./component/UserRoute";
+import Layout from "./pages/global/Layout";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import UserJobsHistory from "./pages/user/UserJobsHistory";
+
+// HOC
+const UserDashboardHOC = Layout(UserDashboard);
+const UserJobsHistoryHOC = Layout(UserJobsHistory);
 
 const App = () => {
   return (
@@ -14,15 +23,34 @@ const App = () => {
       <ToastContainer />
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search/location/:location" element={<Home />} />
-            <Route path="/search/:keyword" element={<Home />} />
-            <Route path="/login" element={<LogIn />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <ProSidebarProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/search/location/:location" element={<Home />} />
+              <Route path="/search/:keyword" element={<Home />} />
+              <Route path="/login" element={<LogIn />} />
+              <Route
+                path="/user/dashboard"
+                element={
+                  <UserRoute>
+                    <UserDashboardHOC />
+                  </UserRoute>
+                }
+              />
+              <Route
+                path="/user/jobs"
+                element={
+                  <UserRoute>
+                    <UserJobsHistoryHOC />
+                  </UserRoute>
+                }
+              />
+
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </ProSidebarProvider>
       </ThemeProvider>
     </>
   );
--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -12,13 +12,20 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import WorkIcon from "@mui/icons-material/Work";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useTheme } from "@emotion/react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { userLogoutAction } from "../redux/actions/userAction";
 
 const pages = ["Home", "Log In"];
 
-function Navbar() {
+const Navbar = () => {
+  //show / hide button
+  const { userInfo } = useSelector((state) => state.signIn);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { palette } = useTheme();
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -37,6 +44,15 @@ function Navbar() {
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
+  };
+
+  // logout function
+  const logoutUser = () => {
+    dispatch(userLogoutAction());
+    window.location.reload(true);
+    setTimeout(() => {
+      navigate("/");
+    }, 500);
   };
 
   return (
@@ -168,7 +184,7 @@ function Navbar() {
               </MenuItem>
 
               {isAuthenticated ? (
-                <MenuItem>
+                <MenuItem onClick={logoutUser}>
                   <Typography
                     style={{
                       textDecoration: "none",
@@ -200,5 +216,5 @@ function Navbar() {
       </Container>
     </AppBar>
   );
-}
+};
 export default Navbar;
